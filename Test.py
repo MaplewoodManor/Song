94b76aef7a2dab9d018d6d6fd094c74f2d9f6781
Glitch <102384025+Steve4Space@users.noreply.github.com>
2023-05-24 10:50:27 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -12,6 +12,18 @@ insertMedia(GREGORYAFTON_CYMBOL, 7, 60)
 makeBeat(OS_KICK01, 7, 1, beat1)
 
 #main 3 tracks
+y = 0
+for x in range(1, 60, 4):
+    fitMedia(MILKNSIZZ_CRASH_PAD_GUITAR, 4, x, (x + 2))
+for x in range(3, 60, 4):
+    fitMedia(AK_UNDOG_ACOUSTIC_GUITAR_2, 5, x, (x+2))
+for x in range(36, 60, 8):
+    fitMedia(JWOLF_COTG_THEME_BASS_CHORUS, 6, x, (x+4))
+for x in range(1, 35, 5):
+    fitMedia(JWOLF_COTG_VOX_MISC_BLOODLINE, 6, x, (x+2))
+fitMedia(GREGORYAFTON_EARBLEED, 8, 55, 60)
+
+
 fitMedia(RD_CINEMATIC_SCORE_STRINGS_4, 1, 2, 61)
 fitMedia(RD_CINEMATIC_SCORE_HARP_4, 2, 2, 61)
 fitMedia(RD_CINEMATIC_SCORE_STRINGS_10, 3, 2, 61)
